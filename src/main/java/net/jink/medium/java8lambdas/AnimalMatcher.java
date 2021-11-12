91b1e778eaecf3ceb9b8bd0895dd410cd3bce9a7
Gillis J. de Nijs <gillis@jink.net>
2021-11-12 02:16:06 +0100

    Step 8: remove custom interface AnimalMatcher

--- src/main/java/net/jink/medium/java8lambdas/AnimalMatcher.java
+++
@@ -1,8 +0,0 @@
-package net.jink.medium.java8lambdas;
-
-import java.util.function.Predicate;
-
-@FunctionalInterface
-public interface AnimalMatcher extends Predicate<Animal> {
-    boolean test(Animal animal);
-}
--- src/main/java/net/jink/medium/java8lambdas/Main.java
+++ src/main/java/net/jink/medium/java8lambdas/Main.java
@@ -2,14 +2,15 @@ package net.jink.medium.java8lambdas;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Animal> animals = Arrays.asList(new Frog(), new Kangaroo(), new Fish());
 
-        AnimalMatcher hopMatcher = Animal::canHop;
-        AnimalMatcher swimMatcher = Animal::canSwim;
+        Predicate<Animal> hopMatcher = Animal::canHop;
+        Predicate<Animal> swimMatcher = Animal::canSwim;
 
         for (Animal animal : animals) {
             if (hopMatcher.test(animal)) {
